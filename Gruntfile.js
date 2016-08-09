7d6036407b1f23b68122c10cf8a8c776c02f6fb6
joshuajosh59 <joshuajosh59@gmail.com>
2016-08-10 00:00:34 +0100

    men a new day

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,8 @@ module.exports = function(grunt) {
         app: {
             source: 'app',
             dist: 'dist',
-            baseurl: 'zeppelin-grunt',
-            git_repo: 'git@github.com:gdg-x/zeppelin-grunt.git',
+            baseurl: 'ngnew',
+            git_repo: 'git@github.com:joshuajosh59/ngnew.git',
             branch: 'gh-pages'
         },
         watch: {
